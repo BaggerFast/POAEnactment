f0af5f3bb7a8cf1f3dcadc9b77ce10bf6d316454
BaggerFast <riosha3@gmail.com>
2022-01-26 13:39:12 +0300

    Первоначальная настройка SQL-SERVER

---
+++ PAODb/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+// Общими сведениями о сборке управляет следующий 
+// набор атрибутов. Измените значения этих атрибутов, чтобы изменить данные,
+// связанные со сборкой SQLCLR.
+[assembly: AssemblyTitle("PAODb")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PAO")]
+[assembly: AssemblyCopyright("Copyright © 2021-2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Сведения о версии для сборки состоят из следующих четырех значений:
+//
+//      Основная версия
+//      Дополнительная версия 
+//      Номер построения
+//      Редакция
+//
+[assembly: AssemblyVersion("0.02.020")]
+[assembly: AssemblyFileVersion("0.02.020")]
