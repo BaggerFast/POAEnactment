e933042dcf4a621a5ec3fcd8cd0ab87902990f1c
BaggerFast <riosha3@gmail.com>
2022-03-14 14:50:29 +0300

    FIX: AutorityMap (Column name was incorrect)

--- PAOCore/DAL/TableModels/AuthorityMap.cs
+++ PAOCore/DAL/TableModels/AuthorityMap.cs
@@ -13,8 +13,8 @@ namespace PAOCore.DAL.TableModels
             LazyLoad();
 
             Id(x => x.Uid).CustomSqlType("UNIQUEIDENTIFIER").Column("UID").Unique().GeneratedBy.Guid().Not.Nullable();
-            Map(x => x.Start).CustomSqlType("DATE").Column("START_ISSUE").Not.Nullable();
-            Map(x => x.End).CustomSqlType("DATE").Column("END_ISSUE").Not.Nullable();
+            Map(x => x.Start).CustomSqlType("DATE").Column("START").Not.Nullable();
+            Map(x => x.End).CustomSqlType("DATE").Column("END").Not.Nullable();
             Map(x => x.Number).CustomSqlType("NVARCHAR(5)").Column("NUMBER").Length(5).Not.Nullable();
 
             References(x => x.Company).Column("COMPANY_UID").Not.Nullable().Unique();
