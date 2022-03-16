ee4a52a5c6d7af4733766a421cac7450f90951f2
Damian Viktorovich Morozov <damian@morozov33.ru>
2022-03-16 11:40:57 +0300

    Updated CompanyEntityTests

--- PAOCoreTests/DAL/TableModels/CompanyEntityTests.cs
+++ PAOCoreTests/DAL/TableModels/CompanyEntityTests.cs
@@ -2,13 +2,20 @@
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
 using NUnit.Framework;
+using PAOCore.DAL.Models;
 using PAOCore.DAL.TableModels;
+using System.Collections.Generic;
 
 namespace PAOCoreTests.DAL.TableModels
 {
     [TestFixture]
     internal class CompanyEntityTests
     {
+        public CompanyEntityTests()
+        {
+            TestsUtils.Setup();
+        }
+
         [Test]
         public void CompanyEntity_Ctor_DoesNotThrow()
         {
@@ -33,6 +40,30 @@ namespace PAOCoreTests.DAL.TableModels
                 if (TestsUtils.AppSettings.DataAccess != null)
                 {
                     CompanyEntity[]? companies = TestsUtils.AppSettings.DataAccess.Crud.GetEntities<CompanyEntity>(null, null);
+                    if (companies != null)
+                    {
+                        foreach (CompanyEntity company in companies)
+                        {
+                            TestContext.WriteLine($"{company}");
+                        }
+                    }
+                }
+            });
+
+            TestsUtils.MethodComplete();
+        }
+
+        [Test]
+        public void CompanyEntity_GetEntitiesWithFilter_DoesNotThrow()
+        {
+            TestsUtils.MethodStart();
+
+            Assert.DoesNotThrow(() =>
+            {
+                if (TestsUtils.AppSettings.DataAccess != null)
+                {
+                    CompanyEntity[]? companies = TestsUtils.AppSettings.DataAccess.Crud.GetEntities<CompanyEntity>(
+                        new FieldListEntity(new Dictionary<string, object> { { "Kpp", "123456789" } }), null);
                     if (companies != null)
                     {
                         foreach (CompanyEntity company in companies)
